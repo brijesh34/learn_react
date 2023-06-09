161b3221cf989b6d603d1e820b51e13aeb375d1a
Brijesh Maurya <98025169+brijesh34@users.noreply.github.com>
2023-06-09 10:02:28 +0530

    commit changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is BRIJESH MAURYA
+          <br/>
+         Hello Guys ,thanks for joining this group.
         </p>
         <a
           className="App-link"
